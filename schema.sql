a96d3d1ffcc06b260cb44b40c63909ac2638929a
chiamyunqing <60507245+chiamyunqing@users.noreply.github.com>
2021-03-26 12:57:53 +0800

    Combine Owns table with Credit Cards table

--- schema.sql
+++ schema.sql
@@ -9,25 +9,20 @@ create table Customers (
     email varchar(100)
 );
 
+/**
+* from_date means when the customer owns this card -> to get active card, find latest from_date
+* For the update_cc function, we just insert a new record into Owns table and CC table respectively.
+* Credit_Cards table enforces the key and total participation constraint to Customers.
+**/
 create table Credit_Cards (
     number varchar(100) primary key,
     CVV varchar(100) not null,
     expiry_date date not null,
-);
-
-/**
-* from_date means when the customer owns this card -> to get active card, find latest from_date
-* For the update_cc function, we just insert a new record into Owns table and CC table respectively.
-* Owns table enforces the key and total participation constraint between Credit Cards and Customers.
-**/
-create table Owns (
-    number varchar(100),
-    cust_id integer not null,
-    from_date date not null,
-    primary key(number),
-    foreign key (number) references Credit_Cards(number),
+    cust_id integer not null, 
+    from_date date not null, /** when the cc is owned**/
     foreign key (cust_id) references Customers(cust_id)
 );
+
 
 /**
 * Registers table stores the sessions that the customer registered for via CC-payment directly.
@@ -40,7 +35,7 @@ create table Registers (
     course_id integer,
     primary key(date, number, sid, launch_date, course_id),
     foreign key (sid, launch_date, course_id) references Sessions(sid, launch_date, course_id),
-    foreign key (number) references Owns(number)
+    foreign key (number) references Credit_Cards(number)
 );
 
 /**
@@ -51,14 +46,11 @@ create table Buys (
     package_id integer,
     number varchar(100), /**cc used to buy packages**/
     num_remaining_redemptions integer,
+    primary key(date, package_id, number),
     foreign key (package_id) references Course_packages(package_id),
-    foreign key (number) references Owns(number),
-    primary key(date, package_id, number)
+    foreign key (number) references Credit_Cards(number)
 );
 
-/**
-* Checking for cancellation requirements will be done in cancel_registration func
-**/
 create table Cancels (
     date date,
     refund_amt float,
